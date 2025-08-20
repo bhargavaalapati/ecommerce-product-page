6f34b72f339aada9b8e7ad146ac773f5d0ac799f
Alapati <ramaalapati0909@gmail.com>
2025-08-20 18:18:37 +0000

    feat: setup basic app structure with a ProductList component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductList = () => {
+    // For now, let's add some placeholder text
+    return (
+        <div className="product-list">
+            <h2>Our Products</h2>
+            <p>This is where the list of products will go.</p>
+        </div>
+    );
+};
+
+export default ProductList;
